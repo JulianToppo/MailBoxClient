c63d0f864f1ef2797bb75278753d03ebbc42216c
JulianToppo <juliantoppo95@gmail.com>
2024-02-27 00:51:41 +0530

    Addition of an inbox which displays all the messages receieved

--- Backend/controller/mails.js
+++ Backend/controller/mails.js
@@ -1,36 +1,51 @@
 const mail = require("../model/mails");
 const user = require("../model/user");
 
-const sendmail= async(req,res)=>{
-    try {
-        const {subject,content,to}= req.body;
-        console.log(subject,content,to)
-        const userFound= await user.findOne({
-            where:{
-                 email:to
-            }
-           
-        })
+const sendmail = async (req, res) => {
+  try {
+    const { subject, content, to } = req.body;
+    console.log(subject, content, to);
+    const userFound = await user.findOne({
+      where: {
+        email: to,
+      },
+    });
 
-        if(!userFound){
-            res.status(500).json({message:"Invalid email id", status:false})
-        }
-        const userCreate=await mail.create({
-            to:userFound.id,
-            content:content,
-            subject:subject,
-            userId:req.user.id,
-        })
-
-        if(userCreate){
-            res.status(201).json({message:"Mail Sent successfully", status:false})
-        }
-      
-    } catch (error) {
-        res.status(500).json({error:error.message, status:false})
+    if (!userFound) {
+      res.status(500).json({ message: "Invalid email id", status: false });
     }
-}
+    const userCreate = await mail.create({
+      to: userFound.id,
+      content: content,
+      subject: subject,
+      userId: req.user.id,
+    });
 
-module.exports={
-    sendmail
-}
+    if (userCreate) {
+      res
+        .status(201)
+        .json({ message: "Mail Sent successfully", status: false });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message, status: false });
+  }
+};
+
+const getAllMails = async (req, res) => {
+  try {
+    const allmail = await mail.findAll({
+      where: {
+        to: req.user.id,
+      },
+    });
+    if (allmail) {
+      res.status(200).json({ mailElements: allmail, status: true });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message, status: false });
+  }
+};
+module.exports = {
+  sendmail,
+  getAllMails,
+};
--- Backend/routes/mail.js
+++ Backend/routes/mail.js
@@ -4,5 +4,6 @@ const authorization = require('../middlewares/authorization')
 const router= express.Router();
 
 router.post('/sendmail',authorization.Authorization,mailController.sendmail)
+router.get('/getallmail',authorization.Authorization,mailController.getAllMails)
 
  module.exports=router;
