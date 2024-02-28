4d0f583aa2f621f63f1780ed7b79c95135d6d7eb
JulianToppo <juliantoppo95@gmail.com>
2024-02-28 20:11:42 +0530

    Handling pages according to userlogin status

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -4,20 +4,36 @@ import Home from "./components/Home";
 import Inbox from "./components/Inbox";
 import ComposeEmail from "./components/ComposeEmail";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import useCrudForMail from "./hooks/useCrudForMail";
+import ErrorPage from "./components/ErrorPage";
 function App() {
   const userStore = useSelector((store) => store.user);
+  const {getUserData}=useCrudForMail();
+
+  useEffect(()=>{
+    if(localStorage.getItem('token')){
+      getUserData();
+    }
+  },[])
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<SignUp />} />
+       
         {userStore.loginStatus && (
           <>
             <Route path="/home" element={<Home />} />
             <Route path="/inbox" element={<Inbox />} />
-            <Route path="/composemail" element={<ComposeEmail />} />
+            <Route path="/composemail" element={<ComposeEmail />}/>
+            <Route path="*" element={<Inbox />} />
+          
           </>
         )}
-        <Route path="*" element={<SignUp />} />
+        {
+          !userStore.loginStatus &&  <Route path="*" element={<SignUp />} />
+        }
+        
       </Routes>
     </div>
   );
